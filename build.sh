eb172211b4c62950d09a827c5fd2596bd87caf8f
BugTheDebugger <bugthedebugger@gmail.com>
2023-06-18 16:37:16 +0530

    updating build script

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 set -e
 # create build folders
 echo "Initializing build paths"
+mkdir -p ./bin
+cd ./bin
+rm -rf mage-icons-react
+git clone git@github.com:Mage-Icons/mage-icons-react.git
+cd ..
 mkdir -p ./bin/mage-icons-react/src/stroke
 mkdir -p ./bin/mage-icons-react/src/bulk
 
